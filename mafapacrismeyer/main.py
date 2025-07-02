52eafbeb2769b401c3f3d738fcc6063741effd9c
Marco Vargas <marcoandres.vargas@alumnos.ulagos.cl>
2025-07-02 15:59:09 -0400

    Update main.py

--- mafapacrismeyer/main.py
+++ mafapacrismeyer/main.py
@@ -1,23 +1,21 @@
 import pygame
 from constantes import *
 from menu import menuPrincipal
-
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_RES)
-    pygame.display.set_caption("MAFAPACRIS MEYER")
-    
+    pygame.display.set_caption(TITULO_JUEGO)
+
+    color_fondo = pygame.Color('#FFFFFF')
+
     running = True
     while running:
-        # El menú principal controla las transiciones
-        action = menuPrincipal(screen)
-        
+        action = menuPrincipal(screen, color_fondo)
         if action == "QUIT":
             running = False
-    
     pygame.quit()
     exit()
 
 if __name__ == "__main__":
     main()
+    
